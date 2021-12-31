fc2035c436faf8ecb6eaa677e79080f24254c360
antonioliverjr <antoniobatistajr@gmail.com>
2021-12-30 23:57:48 -0300

    Adicionado exibição dos contatos na pag principal e visualização contato por id

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='endereco')
+    path('', views.index, name='index'),
+    path('<int:id_contato>', views.view, name='visualizar_contato')
+
 ]
